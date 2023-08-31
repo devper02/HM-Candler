5f7c2538734d45f7532e1de71bdbb4e2c6d12902
devper <oceanfulloffish@gmail.com>
2023-08-31 17:22:32 +0300

    layout finished

--- js/main.js
+++ js/main.js
@@ -6,3 +6,38 @@ menuBtn.addEventListener("click", () => {
   menuMobile.classList.toggle("menu__open");
 });
 // menu-burger/
+
+// swiper slider
+const swiperOne = new Swiper(".feedback__slider", {
+  loop: false,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+const swiperTwo = new Swiper(".certificates-slider", {
+  loop: false,
+  slidesPerView: 3,
+  slidesPerGroup: 4,
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      slidesPerGroup: 4,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+// swiper slider/
+
+// accardeon
+const accardeonCloseNode = document.querySelectorAll("#acc-close");
+
+accardeonCloseNode.forEach(function (i) {
+  i.addEventListener("click", function () {
+    const nextElement = i.nextElementSibling;
+    if (nextElement) {
+      nextElement.classList.toggle("hidden");
+    }
+  });
+});
